ae2445effb1e59aaf7723180213e890dcdac0bf3
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2025-11-11 13:43:43 +0800

    Add calculite project and modify project card CSS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -270,7 +270,7 @@ onMounted(() => {
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
+        justify-content: flex-start;
         background-color: var(--md-sys-color-primary-container);
         color: var(--md-sys-color-on-primary-container);
         border-radius: 25px;
